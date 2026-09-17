5e209b5ff1c9ee8e99f4891fc8cab0300ca8db60
AndreSoderlund <andre.soderlund@b3.se>
2026-09-17 07:43:29 +0200

    Compare attachment content by value in AssetAttachmentContent

---
+++ src/test/java/se/sundsvall/partyassets/service/AssetAttachmentContentTest.java
@@ -0,0 +1,30 @@
+package se.sundsvall.partyassets.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AssetAttachmentContentTest {
+
+	private static final String FILE_NAME = "lokalritning.pdf";
+	private static final String MIME_TYPE = "application/pdf";
+
+	@Test
+	void equalsAndHashCodeConsidersContent() {
+		final var content = new AssetAttachmentContent(FILE_NAME, MIME_TYPE, "content".getBytes());
+		final var equalContent = new AssetAttachmentContent(FILE_NAME, MIME_TYPE, "content".getBytes());
+		final var otherContent = new AssetAttachmentContent(FILE_NAME, MIME_TYPE, "other".getBytes());
+
+		assertThat(content).isEqualTo(content).isEqualTo(equalContent).isNotEqualTo(otherContent).isNotEqualTo(null).isNotEqualTo(FILE_NAME);
+		assertThat(content).hasSameHashCodeAs(equalContent);
+		assertThat(content.hashCode()).isNotEqualTo(otherContent.hashCode());
+	}
+
+	@Test
+	void toStringShowsContentSize() {
+		assertThat(new AssetAttachmentContent(FILE_NAME, MIME_TYPE, "content".getBytes()))
+			.hasToString("AssetAttachmentContent[fileName=lokalritning.pdf, mimeType=application/pdf, content=7 bytes]");
+		assertThat(new AssetAttachmentContent(FILE_NAME, MIME_TYPE, null))
+			.hasToString("AssetAttachmentContent[fileName=lokalritning.pdf, mimeType=application/pdf, content=0 bytes]");
+	}
+}
